03e236096bc562f3b5546cbca8a9da57ece7a364
SoloPython <alexander.eric.3@gmail.com>
2023-04-10 02:49:02 -0500

    first commit

--- core/asgi.py
+++ core/asgi.py
@@ -1,9 +1,9 @@
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter
 
 application = ProtocolTypeRouter({
